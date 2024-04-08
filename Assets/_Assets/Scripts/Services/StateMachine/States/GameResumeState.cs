8397590cfef9caff9fb728043631cb5558852232
R1nge <r1ngegamedev@gmail.com>
2024-04-08 11:47:20 +0300

    Fixed timer starts when back from pause menu

--- Assets/_Assets/Scripts/Services/StateMachine/States/GameResumeState.cs
+++ Assets/_Assets/Scripts/Services/StateMachine/States/GameResumeState.cs
@@ -1,4 +1,5 @@
 ﻿using _Assets.Scripts.Services.Audio;
+using _Assets.Scripts.Services.GameModes;
 using _Assets.Scripts.Services.UIs.StateMachine;
 using Cysharp.Threading.Tasks;
 
@@ -10,20 +11,28 @@ namespace _Assets.Scripts.Services.StateMachine.States
         private readonly UIStateMachine _uiStateMachine;
         private readonly IAudioSettingsLoader _audioSettingsLoader;
         private readonly TimeRushTimer _timeRushTimer;
+        private readonly GameModeService _gameModeService;
 
-        public GameResumeState(PlayerInput playerInput, UIStateMachine uiStateMachine, IAudioSettingsLoader audioSettingsLoader, TimeRushTimer timeRushTimer)
+        public GameResumeState(PlayerInput playerInput, UIStateMachine uiStateMachine,
+            IAudioSettingsLoader audioSettingsLoader, TimeRushTimer timeRushTimer, GameModeService gameModeService)
         {
             _playerInput = playerInput;
             _uiStateMachine = uiStateMachine;
             _audioSettingsLoader = audioSettingsLoader;
             _timeRushTimer = timeRushTimer;
+            _gameModeService = gameModeService;
         }
 
         public async UniTask Enter()
         {
             await _uiStateMachine.SwitchState(UIStateType.Game);
             await UniTask.DelayFrame(1);
-            _timeRushTimer.Resume();
+            
+            if (_gameModeService.SelectedGameMode == GameModeService.GameMode.TimeRush)
+            {
+                _timeRushTimer.Resume();
+            }
+
             _playerInput.Enable();
         }
 
